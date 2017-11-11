e20d26657c502f1a29f086a8f6ab96923b4f444c
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-11 16:54:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,10 @@ function logWhisper (string) {
 
 
 function sayHiToGrandma (string) {
-  if ('string' === 'string'.toLowerCase()) {
+  if ('string' === 'string.toLowerCase()') {
   return 'I can\'t hear you!';
-  break;
-}
 
+}
   else if ('string' === 'string'.toUpperCase()) {
   return "YES INDEED!"
 }
