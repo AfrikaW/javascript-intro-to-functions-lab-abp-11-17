2fca0030189279de0aeeb30176ae280dc25ef79d
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-11 16:23:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ function sayHiToGrandma (string) {
   if (`${string}` === `${string}`.toLowerCase()) {
   return 'I can\'t hear you!'()
 }
-break;
+
 else if (`${string}` === `${string}`.toUpperCase()) {
 return "YES INDEED!"();
 }
-break;
+
 else if (`${string}` ==='I love you, Grandma.') {
   return "I love you, too"();
 }
