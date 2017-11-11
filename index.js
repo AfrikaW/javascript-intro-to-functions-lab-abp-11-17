41b58093f9722bd949550fd35abb448720ebd334
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-11 15:33:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
-const string= string.toUpperCase
+Let string
 function logShout(string) {
-  console.log(`${string}`)
+  console.log(`${string}.toUpperCase`)
 }
